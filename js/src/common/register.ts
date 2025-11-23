ce1a09582d10494e09aa602a0b7f56a2698ee0f6
Lady-Byron <samantha-everett@outlook.com>
2025-11-23 03:58:24 -0500

    Update register.ts

--- js/src/common/register.ts
+++ js/src/common/register.ts
@@ -4,6 +4,12 @@ import TrendsWidget from './components/widget';
 import { extName } from '../r';
 
 export default function (app: Application) {
+  // [权限控制]
+  // 如果当前用户（或游客）没有浏览论坛的权限，直接中止，不注册挂件。
+  if (!app.forum.attribute('canViewForum')) {
+    return;
+  }
+
   new Widgets()
     .add({
       key: 'liplum-trends-widget',
